f68d1c07fba529949121ad0f0f0699915c948fe3
Tusked-D <orlando.paz@uadec.edu.mx>
2025-10-23 19:17:57 -0600

    Spring inicial
    Cree el proyecto en IntelliJ IDEA Ultimate

---
+++ AgenciaServicios/src/main/java/com/agenciaservicios/agenciaservicios/AgenciaServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.agenciaservicios.agenciaservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenciaServiciosApplication.class, args);
+    }
+
+}
---
+++ AgenciaServicios/src/test/java/com/agenciaservicios/agenciaservicios/AgenciaServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaservicios.agenciaservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaServiciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
