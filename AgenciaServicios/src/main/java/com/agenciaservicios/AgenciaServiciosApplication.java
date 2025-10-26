2f0f9c9b162ca2df6d2fb4317c863e2b6c37751c
Tusked-D <orlando.paz@uadec.edu.mx>
2025-10-25 20:11:31 -0600

    Tabla cliente implementada
    Agrega las clases de clientes controller, service y models, configure el xml y las propiedades de la app, implemente el spring data jpa

--- AgenciaServicios/src/main/java/com/agenciaservicios/AgenciaServiciosApplication.java
+++ AgenciaServicios/src/main/java/com/agenciaservicios/AgenciaServiciosApplication.java
@@ -1,4 +1,4 @@
-package com.agenciaservicios.agenciaservicios;
+package com.agenciaservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
